6a04149bfb13950d39771b4ae1721dca725ca3a6
Mihir Shah <mihirshah.11204@gmail.com>
2022-04-28 13:10:13 -0500

    update

--- openjpeg/src/lib/openjp2/last.sh
+++ openjpeg/src/lib/openjp2/last.sh
@@ -1,5 +1,9 @@
 
-RLWASM_DIR="../../../../rlbox_wasm2c_sandbox"
+rm libWasmJP2.{c,h} mihir.wasm libopenjp2.so 
+
+ROOT="/home/mihirs/nets/imagemagick-361s"
+
+RLWASM_DIR="$ROOT/rlbox_wasm2c_sandbox"
 CC="${RLWASM_DIR}/build/_deps/wasiclang-src/bin/clang"
 CFLAGS="--sysroot ${RLWASM_DIR}/build/_deps/wasiclang-src/share/wasi-sysroot -fPIC -O0 -g"
 LD="${RLWASM_DIR}/build/_deps/wasiclang-src/bin/wasm-ld"
@@ -11,8 +15,8 @@ $RLWASM_DIR/build/_deps/mod_wasm2c-src/bin/wasm2c -o libWasmJP2.c mihir.wasm
 
 echo "Produced .wasm/.c file."
 
-WASM_DIR="/home/mihirs/nets/rlbox_wasm2c_sandbox/build/_deps/mod_wasm2c-src/wasm2c"
-LIB_DIR="/home/mihirs/nets/openjpeg/src/lib/openjp2"
+WASM_DIR="$ROOT/rlbox_wasm2c_sandbox/build/_deps/mod_wasm2c-src/wasm2c"
+LIB_DIR="$ROOT/openjpeg/src/lib/openjp2"
 
 cd $WASM_DIR
 gcc -shared -fPIC -O0 -g -o libopenjp2.so $LIB_DIR/libWasmJP2.c $WASM_DIR/wasm-rt-impl.c $WASM_DIR/wasm-rt-os-unix.c $WASM_DIR/wasm-rt-os-win.c $WASM_DIR/wasm-rt-wasi.c -I .
@@ -21,8 +25,8 @@ cd -
 
 echo "Produced .so file."
 
-g++ -std=c++17 -O0 -g rlbox_example.cpp -o example -I /home/mihirs/nets/rlbox_wasm2c_sandbox/build/_deps/rlbox-src/code/include -I . -I /home/mihirs/nets/rlbox_wasm2c_sandbox/include/ -I /home/mihirs/nets/rlbox_wasm2c_sandbox/build/_deps/mod_wasm2c-src/wasm2c/ -lpthread -ldl -lrt
+g++ -std=c++17 -O0 -g rlbox_example.cpp -o example -I $RLWASM_DIR/build/_deps/rlbox-src/code/include -I . -I $RLWASM_DIR/include/ -I $RLWASM_DIR/build/_deps/mod_wasm2c-src/wasm2c/ -pthread -lpthread -ldl -lrt
 
 echo "Produced rlbox file."
 
-gdb ./example
+./example
