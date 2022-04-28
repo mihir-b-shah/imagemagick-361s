b81ab3156d04485eaf5b826ad96c522fc0283c15
Mihir Shah <mihirshah.11204@gmail.com>
2022-04-28 16:29:31 -0500

    linkage errors

--- ImageMagick/conf.sh
+++ ImageMagick/conf.sh
@@ -1,1 +1,11 @@
-./configure CC=g++ CFLAGS=-std=c++17 --without-magick-plus-plus --without-perl --without-dps --without-freetype --without-jbig --without-png --without-jpeg --without-lcms --without-lcms2 --without-lzma --without-tiff --without-wmf --without-xml --without-bzlib --disable-openmp --without-x --prefix=/home/mihirs/nets/ImageMagick/build/ --with-openjp2=yes
+
+ROOT="/home/mihirs/nets/imagemagick-361s"
+OPJ_DIR="${ROOT}/openjpeg/src/lib/openjp2"
+
+CC=g++                                                                                    \
+CFLAGS="-std=c++17 -I ${ROOT}/rlbox_wasm2c_sandbox/build/_deps/rlbox-src/code/include"    \
+LDFLAGS="-L${OPJ_DIR} -Wl,-rpath=${OPJ_DIR} -lopenjp2"                                    \
+LIBOPENJP2_CFLAGS="-I${OPJ_DIR}"                                                          \
+LIBOPENJP2_LIBS="-L${OPJ_DIR} -Wl,-rpath=${OPJ_DIR} -lopenjp2"                            \
+./configure --without-magick-plus-plus --without-perl --without-dps --without-freetype --without-jbig --without-png --without-jpeg --without-lcms --without-lcms2 --without-lzma --without-tiff --without-wmf --without-xml --without-bzlib --disable-openmp --without-x --prefix=/home/mihirs/nets/ImageMagick/build/ --with-openjp2=yes
+
--- openjpeg/src/lib/openjp2/rlbox_example.cpp
+++ openjpeg/src/lib/openjp2/rlbox_example.cpp
@@ -13,9 +13,9 @@ int main()
    rlbox_sandbox<rlbox_wasm2c_sandbox> sandbox;
    sandbox.create_sandbox("./libopenjp2.so");
    // Invoke function bar with parameter 1
-   auto tainted_ncpus = sandbox.invoke_sandbox_function(opj_get_num_cpus);
-   int n_cpus = tainted_ncpus.copy_and_verify([](int fn){ return fn; });
-   printf("n_cpus: %d\n", n_cpus);
+   auto tainted_version = sandbox.invoke_sandbox_function(opj_version);
+   const char* version = tainted_version.UNSAFE_unverified();
+   printf("%s\n", version);
    sandbox.destroy_sandbox();
    return 0;
 }
