692bd378524b8ab23bcd3ed79643ea1898e4e0a4
Mihir Shah <mihirshah.11204@gmail.com>
2022-05-12 12:24:21 -0500

    gets to Writejp2image for clamp

--- openjpeg/src/lib/openjp2/opj_stdint.h
+++ openjpeg/src/lib/openjp2/opj_stdint.h
@@ -40,10 +40,10 @@ typedef   signed __int8   int8_t;
 typedef unsigned __int8   uint8_t;
 typedef   signed __int16  int16_t;
 typedef unsigned __int16  uint16_t;
-typedef   signed __int32  int32_t;
-typedef unsigned __int32  uint32_t;
-typedef   signed __int64  int64_t;
-typedef unsigned __int64  uint64_t;
+typedef   signed __int32  int;
+typedef unsigned __int32  unsigned int;
+typedef   signed __int64  long long;
+typedef unsigned __int64  unsigned long long;
 #else
 #error unsupported platform
 #endif
